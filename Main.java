33fadacd2365a4342f7d51106df77b91a489d8b9
Subodh Nandi <subodhnandi59428@gmail.com>
2025-07-28 11:09:00 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.subodh.mainclass;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.subodh.SpringBeans.Student;
+import com.subodh.cfg.JavaConfiguration;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=new AnnotationConfigApplicationContext(JavaConfiguration.class);
+		Student std=(Student) ctx.getBean(Student.class);
+		std.display();
+	}
+
+}
